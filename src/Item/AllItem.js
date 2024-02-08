6f8af37903384e946840b4f9f62608c5dc3745f4
Francesca <francescaprt@yahoo.com>
2024-02-08 14:30:18 +0100

    filtro per prezzo

--- src/Item/AllItem.js
+++ src/Item/AllItem.js
@@ -1,9 +1,17 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import ItemOverview from "./ItemOverview";
 
 export default function AllItem(props) {
   const [items, setItems] = useState([]);
+
+  const [flicker, setFlicker] = useState(false);
+
+  const [minPr, setMin] = useState(1);
+  const [maxPr, setMax] = useState(100);
+  const nomIn = useRef(null);
+  const minIn = useRef(null);
+  const maxIn = useRef(null);
 
   useEffect(() => {
     axios.get("/items").then((response) => {
@@ -21,16 +29,76 @@ export default function AllItem(props) {
         setItems(clone);
       })
       .catch(() => {
-        alert("No, ha dei figli, chi penserà ai bambini");
+        alert("Non è possibile eliminare prodotti con recensioni");
       });
+  }
+
+  function isShowable(i, nom, minPrice, maxPrice) {
+    if (nom && !i.name.toLowerCase().includes(nom.toLowerCase())) return false;
+
+    if (i.price < minPrice || i.price > maxPrice) return false;
+
+    return true;
   }
 
   return (
     <>
+      <div className="row gy-5">
+        <div className="col-3 p-4">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">
+              Nominativo
+            </span>
+            <input
+              type="text"
+              ref={nomIn}
+              class="form-control"
+              onChange={() => setFlicker(!flicker)}
+            />
+          </div>
+          <label for="customRange1" class="form-label">
+            Prezzo min {minPr}
+          </label>
+          <input
+            type="range"
+            ref={minIn}
+            min={1}
+            max={100}
+            defaultValue={1}
+            class="form-range"
+            id="customRange1"
+            onChange={(e) => setMin(e.target.value)}
+          />
+          <label for="customRange1" class="form-label">
+            Prezzo max {maxPr}
+          </label>
+          <input
+            type="range"
+            ref={maxIn}
+            min={1}
+            max={100}
+            class="form-range"
+            id="customRange1"
+            onChange={(e) => setMax(e.target.value)}
+          />
+          <br />
+          <br />
+        </div>
+      </div>
+
       <div className="row">
-        {items.map((i) => (
-          <ItemOverview {...i} deleteMe={deleteItem} />
-        ))}
+        {items
+          .filter((p) =>
+            isShowable(
+              p,
+              nomIn.current.value,
+              minIn.current.value,
+              maxIn.current.value
+            )
+          )
+          .map((i) => (
+            <ItemOverview {...i} deleteMe={deleteItem} />
+          ))}
       </div>
     </>
   );
